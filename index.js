9600960d0df30d47124a1cc71c2b7cc1c705f671
Uzair Ahmad <154722187+uzairatta@users.noreply.github.com>
2026-05-18 17:17:13 +0500

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { ConnectMongoDb } from "./Utils/mongodb.js";
-import URLRoute from "./Routes/urls.js";
+import URLRoute from "./Routes/url.js";
 
 dotenv.config();
 
