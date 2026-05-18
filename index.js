55d90de510f02948d4644ef00177d9d3654d52f0
Uzair Ahmad <154722187+uzairatta@users.noreply.github.com>
2026-05-18 17:03:11 +0500

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import { ConnectMongoDb } from "./Utils/mongodb.js";
-import URLRoute from "./Routes/url.js";
+import URLRoute from "./Routes/urls.js";
 
 dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*"
+}));
+
 app.use(express.json());
 
 ConnectMongoDb();
