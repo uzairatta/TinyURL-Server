9a1ad97ddffdae7fc98f3a49efdc9b044c5af35f
Uzair Ahmad <154722187+uzairatta@users.noreply.github.com>
2026-05-21 08:46:32 +0500

    Update RedirectURL.js

--- Controllers/RedirectURL.js
+++ Controllers/RedirectURL.js
@@ -1,14 +1,23 @@
+import { parse } from "dotenv";
 import { URLs } from "../Models/urls.js";
+
+import { getCache, setCache } from "../Utils/redis.js";
 
 export const RedirectURL = async (req, res) => {
   const { shortId } = req.params;
   try {
-    console.log(shortId);
-    const url = await URLs.findOne({ shortId: shortId });
-    if (!url) return res.status(404).json({ ok: false });
-    res.redirect(url.longUrl);
+    const urlFromCache = await getCache(shortId);
+
+    if (urlFromCache) {
+      res.redirect(JASON.parse(urlFromCache));
+      return;
+    }
+
+    const resUrls = await URLs.find({ shortId: shortId });
+    const element = resUrls[0];
+    await setCache(shortId, element.longUrl, 7400); // 2 hours
+    res.redirect(element.longUrl);
   } catch (err) {
-    console.log(err);
     res.status(500).json({
       ok: false,
     });
