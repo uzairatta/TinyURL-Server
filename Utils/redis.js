eacb3a1ac321ec8b8fc086e6239e6464c3f946d1
Uzair Ahmad <154722187+uzairatta@users.noreply.github.com>
2026-05-21 08:46:01 +0500

    Update redis.js

--- Utils/redis.js
+++ Utils/redis.js
@@ -1,9 +1,38 @@
-import { createClient } from "redis";
+import { createClient } from 'redis';
+import dotenv from 'dotenv';
+dotenv.config();
 
-const client = createClient();
+const redisClient = createClient({
+  username: process.env.REDIS_USERNAME,
+  password: process.env.REDIS_PASSWORD,
+  socket: {
+    host: process.env.REDIS_HOST,
+    port: Number(process.env.REDIS_PORT),
+  },
+});
 
-client.on("error", (err) => console.log("Redis Client Error", err));
+redisClient.on('error', (err) => console.error('Redis Client Error', err));
 
-await client.connect();
+export const connectRedis = async () => {
+  if (!redisClient.isOpen) {
+    await redisClient.connect();
+  }
+  console.log("Connected to Redis");
+};
 
-export default client;
+export const setCache = async (key, value, expireSeconds = 3600) => {
+  try {
+    await redisClient.set(key, JSON.stringify(value), {
+      EX: expireSeconds,
+    });
+  } catch(err) {
+    console.log(err)
+  }
+};
+
+export const getCache = async (key) => {
+  const data = await redisClient.get(key);
+  return data ? JSON.parse(data) : null;
+};
+
+export default redisClient;
